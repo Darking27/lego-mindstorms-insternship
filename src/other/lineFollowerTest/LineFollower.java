72a3d5e70527dc5cb22fe97f2772acce4b58d5db
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-02 18:20:17 +0100

    Added drive forward method

--- src/other/lineFollowerTest/LineFollower.java
+++ src/other/lineFollowerTest/LineFollower.java
@@ -6,14 +6,20 @@ import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
 
 public class LineFollower {
+	
+	 int SAMPLE_SPEED = 50;              // in ms
     
     ColorID LINE_COLOR = ColorID.WHITE;
+    ColorID BACKGROUND_COLOR = ColorID.BLACK;
+    ColorID FINISH_COLOR = ColorID.BLUE;
     
     Brick brick;
     ColorSensor colorSensor;
 
     RegulatedMotor leftMotor;
     RegulatedMotor rightMotor;
+    
+    LineFollowerState state;
     
     public LineFollower(Brick brick, String colorSensorPort, String leftMotorPort, String rightMotorPort) {
         this.brick = brick;
@@ -22,31 +28,21 @@ public class LineFollower {
         
         this.rightMotor = new EV3LargeRegulatedMotor(brick.getPort(rightMotorPort));
         this.leftMotor = new EV3LargeRegulatedMotor(brick.getPort(leftMotorPort));
+        
+        state = LineFollowerState.DRIVE_FORWARD_GAP;
     }
     
     public void followLine() {
-        LineFollowerState state = LineFollowerState.LINE_LOST;
-        colorSensor.start();
-        //Drive 10cm forward
         
-        while (!state.equals(LineFollowerState.DONE)) {
-            if (state.equals(LineFollowerState.LINE_LOST)) {
-                while (state.equals(LineFollowerState.LINE_LOST)) {
-                    
-                }
-            }
-        }
     }
     
     
     //Motor functions
     
-    int ROTATE_100_DEG_TIME = 5;        // time it takes the robot to turn 100 degrees (in seconds) 
-    
-    int SAMPLE_SPEED = 50;              // in ms
+    int ROTATE_100_DEG_TIME = 2000;        // time it takes the robot to turn 100 degrees (in ms) 
     
     public boolean checkForLine(ColorSensor colorSensor, int degree, boolean right) {
-        int count = ROTATE_100_DEG_TIME * 1000 / SAMPLE_SPEED;
+        int count = ROTATE_100_DEG_TIME / SAMPLE_SPEED;
         
         RegulatedMotor m1;
         RegulatedMotor m2;
@@ -78,5 +74,29 @@ public class LineFollower {
         m2.stop();
         
         return false;
+    }
+    
+    int DRIVE_1CM_TIME = 2000;		// time it takes the robot to drive 1 cm (in ms)
+    
+    public boolean driveForward(ColorSensor colorSensor, int distance) {
+    	int count = DRIVE_1CM_TIME / SAMPLE_SPEED;
+    	
+    	this.leftMotor.setSpeed(300);
+    	this.rightMotor.setSpeed(300);
+    	
+    	for (int i = 0; i < count; i++) {
+            ColorID color = colorSensor.getColorId();
+            if (color.equals(LINE_COLOR)) {
+                this.leftMotor.stop();
+                this.rightMotor.stop();
+                return true;
+            }
+            Delay.msDelay(SAMPLE_SPEED);
+        }
+    	
+    	this.leftMotor.stop();
+        this.rightMotor.stop();
+    	
+    	return false;
     }
 }
