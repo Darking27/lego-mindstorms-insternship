ca2ae4da21a43ea840e2ace473d278bf94454e59
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-04 22:14:19 +0100

    Added drive test

---
+++ src/lineFollower/DriveTest.java
@@ -0,0 +1,44 @@
+package lineFollower;
+
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
+public class DriveTest {
+
+	Brick brick;
+	
+	RegulatedMotor leftMotor;
+    RegulatedMotor rightMotor;
+	
+	public DriveTest() {
+		brick = BrickFinder.getDefault();
+		
+		leftMotor = new EV3LargeRegulatedMotor(brick.getPort("A"));
+		rightMotor = new EV3LargeRegulatedMotor(brick.getPort("B"));
+	}
+	
+	public static void main(String[] args) {
+		DriveTest test = new DriveTest();
+
+		test.runTest();
+	}
+	
+	public void runTest() {
+		System.out.println("Drive 10s forward");
+		
+		this.leftMotor.setSpeed(300);
+    	this.rightMotor.setSpeed(300);
+    	
+    	this.rightMotor.forward();
+    	this.leftMotor.forward();
+    	
+    	Delay.msDelay(10000);
+    	
+    	this.leftMotor.stop();
+    	this.rightMotor.stop();
+	}
+
+}
