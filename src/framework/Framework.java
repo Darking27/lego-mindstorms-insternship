d5b82dc4f6156d43f360bfa7b0f4a70b72b8f57a
Samuel <samuelborn@outlook.de>
2021-11-08 08:21:44 +0100

    FrameWork multithreading

--- src/framework/Framework.java
+++ src/framework/Framework.java
@@ -25,10 +25,31 @@ public class Framework {
 	public static void main(String[] args) {
 		Brick brick = BrickFinder.getDefault();
 		Key stop_programm_key = brick.getKey("Enter");
+		Key menu_key = brick.getKey("Escape");
+		
+		WalkerThread walkerThread = new WalkerThread(chooseParcoursSection());
+		walkerThread.start();
 
-		// displays the menu with all entries from Parcours Section
-		// when the given element is part of the parcours the correct start index is returned
-		// else just the selected section will be walked
+		while (!stop_programm_key.isDown()) {
+			if (menu_key.isDown()) {
+				walkerThread.stop();
+				walkerThread = new WalkerThread(chooseParcoursSection());
+				walkerThread.start();
+			}
+		}
+	}
+
+	/**
+	 * @return the index of the choosen Parcours Section
+	 * 
+	 * so if the parcours is LINE -> BOX -> BRIDGE
+	 * and the user chooses BOX
+	 * this method returns 1
+	 */
+	private static int chooseParcoursSection() {
+		// displays the menu with all entries from parcours Section
+		// when the given element is part of the parcours the correct start 
+		// index is returned
 		String[] menuItems = new String[ParcoursSection.values().length];
 		for (int i = 0; i < ParcoursSection.values().length; i++) {
 			menuItems[i] = ParcoursSection.values()[i].name();
@@ -36,15 +57,19 @@ public class Framework {
 		TextMenu textMenu = new TextMenu(menuItems);
 		int pressed_index = textMenu.select();
 		ParcoursSection start_section = ParcoursSection.valueOf(menuItems[pressed_index]);
-		int parcours_section_index = parcours_section_order.indexOf(start_section);
-		if(parcours_section_index == -1) { // section not part of the parcours
-			start_section.start_walking();
+		return parcours_section_order.indexOf(start_section);
+		
+	}
+
+	public static class WalkerThread extends Thread {
+		int parcours_section_index;
+
+		public WalkerThread(int parcours_section_index) {
+			this.parcours_section_index = parcours_section_index;
 		}
 
-		// TODO: MENU KEY DETECTION DOESNT WORK YET BECAUSE WHILE DOESNT LOOP -- MULTITHREADING
-		while (!stop_programm_key.isDown()) {
-			// starts with the starting index and then follow the order defined in parcours
-			// order
+		@Override
+		public void run() {
 			for (int i = parcours_section_index; i < parcours_section_order.size(); i++) {
 				parcours_section_order.get(i).start_walking();
 				// once the start walking method returns (i.e. the robot has seen a blue stripe
@@ -52,5 +77,4 @@ public class Framework {
 			}
 		}
 	}
-
 }
