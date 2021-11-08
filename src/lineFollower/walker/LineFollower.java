8f6280c8a543cbbc9207481543f0d091812e548b
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-08 17:49:30 +0100

    Added line follower walker

---
+++ src/lineFollower/walker/LineFollower.java
@@ -0,0 +1,16 @@
+package lineFollower.walker;
+
+import framework.ParcoursWalkable;
+import framework.WalkableStatus;
+
+public class LineFollower implements ParcoursWalkable {
+
+	@Override
+	public WalkableStatus start_walking() {
+		return null;
+	}
+
+	private boolean handleStart() {
+		return false;
+	}
+}
