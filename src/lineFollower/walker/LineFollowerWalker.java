d7938627dc828098116700b26f6559e53c5c5671
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-08 22:20:14 +0100

    implemented line follower walker

---
+++ src/lineFollower/walker/LineFollowerWalker.java
@@ -0,0 +1,10 @@
+package lineFollower.walker;
+
+public class LineFollowerWalker {
+
+	public static void main(String[] args) {
+		LineFollower lineFollower = new LineFollower();
+		lineFollower.start_walking();
+	}
+
+}
---
+++ src/lineFollower/walker/State.java
@@ -0,0 +1,5 @@
+package lineFollower.walker;
+
+public enum State {
+	START, SEARCH_LINE, DONE, FOLLOW_LINE, SEARCH_LINE_BORDER_LEFT, GAP, OBSTACLE
+}
