2090a20d5bb2676c9d1a9834d532a79f05f4b1b5
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-12 12:29:22 +0100

    removed state ENTER_PRESSED

--- src/lineFollower/walker/stateMachine/StateName.java
+++ src/lineFollower/walker/stateMachine/StateName.java
@@ -2,9 +2,9 @@ package lineFollower.walker.stateMachine;
 
 public enum StateName {
     START,
-    ENTER_PRESSED,
     OBSTACLE,
     SEARCH_LINE,
     FOLLOW_LINE,
-    GAP
+    GAP,
+    // SEARCH_LEFT_LINE_BORDER		// Maybe not even needed
 }
