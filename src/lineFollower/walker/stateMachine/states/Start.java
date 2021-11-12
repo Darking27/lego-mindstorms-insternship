da60bf8fd8ee6c13b48e38b8081abcd57eecabea
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-12 15:50:56 +0100

    used turn method defined in BaseState + refectoring

--- src/lineFollower/walker/stateMachine/states/Start.java
+++ src/lineFollower/walker/stateMachine/states/Start.java
@@ -1,7 +1,5 @@
 package lineFollower.walker.stateMachine.states;
 
-import framework.Ports;
-import lejos.robotics.RegulatedMotor;
 import lineFollower.walker.stateMachine.FinishLineException;
 import lineFollower.walker.stateMachine.ProcessInteruptedEnterException;
 import lineFollower.walker.stateMachine.RobotCollisionException;
@@ -15,60 +13,17 @@ public class Start extends BaseState {
     }
 
     @Override
-    public StateName handleState() throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
+    public StateName handleState()
+    		throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
         
         logCurrentState();
         
         driveForwardStraight(ENCODER_GAP_DISTANCE, false);
         
-        calibrateFilter(ENCODER_TURN_45, true);
-        calibrateFilter(2 * ENCODER_TURN_45, false);
-        calibrateFilter(ENCODER_TURN_45, true);
+        turnRight(ENCODER_TURN_45, false);
+        turnRight(2 * ENCODER_TURN_45, false);
+        turnRight(ENCODER_TURN_45, false);
         
         return StateName.SEARCH_LINE;
-    }
-    
-    private void calibrateFilter(int encoderValue, boolean rightTurn) throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
-        float[] sample = new float[autoAdjustRGBFilter.sampleSize()];
-        
-        Ports.LEFT_MOTOR.resetTachoCount();
-        Ports.RIGHT_MOTOR.resetTachoCount();
-        
-        RegulatedMotor m1;
-        RegulatedMotor m2;
-        
-        if (rightTurn) {
-            m1 = Ports.LEFT_MOTOR;
-            m2 = Ports.RIGHT_MOTOR;
-        } else {
-            m1 = Ports.RIGHT_MOTOR;
-            m2 = Ports.LEFT_MOTOR;
-        }
-        
-        int m1TachoCount = 0;
-        int m2TachoCount = 0;
-        
-        Ports.LEFT_MOTOR.setSpeed(400);
-        Ports.RIGHT_MOTOR.setSpeed(400);
-        
-        m1.forward();
-        m2.backward();
-        
-        while (m1TachoCount <= encoderValue || m2TachoCount <= encoderValue) {
-        	autoAdjustRGBFilter.fetchSample(sample, 0);
-        	
-        	if (m1TachoCount >= encoderValue) {
-                m1.stop(true);
-            }
-            if (m2TachoCount >= encoderValue) {
-                m2.stop(true);
-            }
-            checkRobotInputs(sample);
-            m1TachoCount = Math.abs(m1.getTachoCount());
-            m2TachoCount = Math.abs(m2.getTachoCount());
-        }
-        
-        m1.stop(true);
-        m2.stop(true);
     }
 }
