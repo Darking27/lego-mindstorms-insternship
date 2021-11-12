5340b37e7c5f1536b6c04e91c6580b3a06abe79f
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-12 12:30:39 +0100

    Added state Gap to line follower state machine

---
+++ src/lineFollower/walker/stateMachine/states/Gap.java
@@ -0,0 +1,24 @@
+package lineFollower.walker.stateMachine.states;
+
+import lineFollower.walker.stateMachine.FinishLineException;
+import lineFollower.walker.stateMachine.ProcessInteruptedEnterException;
+import lineFollower.walker.stateMachine.RobotCollisionException;
+import lineFollower.walker.stateMachine.StateName;
+
+public class Gap extends BaseState {
+	
+	public Gap() {
+		super();
+		this.stateName = StateName.GAP;
+	}
+	
+	@Override
+	public StateName handleState() throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
+		if (driveForwardStraight(ENCODER_GAP_DISTANCE, true)) {
+			// Line found
+			return StateName.FOLLOW_LINE;
+		}
+		// Line not found
+		return StateName.SEARCH_LINE;
+	}
+}
