8adde760410f260b54cdc186734d624b3d43d43a
Felix Rohn <felix.rohn@student.kit.edu>
2021-11-12 16:15:18 +0100

    added State objectes to stateName

--- src/lineFollower/walker/stateMachine/StateName.java
+++ src/lineFollower/walker/stateMachine/StateName.java
@@ -1,10 +1,26 @@
 package lineFollower.walker.stateMachine;
 
+import lineFollower.walker.stateMachine.exceptions.FinishLineException;
+import lineFollower.walker.stateMachine.exceptions.ProcessInteruptedEnterException;
+import lineFollower.walker.stateMachine.exceptions.RobotCollisionException;
+import lineFollower.walker.stateMachine.exceptions.RobotErrorException;
+import lineFollower.walker.stateMachine.states.*;
+
 public enum StateName {
-    START,
-    OBSTACLE,
-    SEARCH_LINE,
-    FOLLOW_LINE,
-    GAP,
-    // SEARCH_LEFT_LINE_BORDER		// Maybe not even needed
+    START(new StartState()),
+    OBSTACLE(new ObstacleState()),
+    SEARCH_LINE(new SearchLineState()),
+    FOLLOW_LINE(new FollowLineState()),
+    GAP(new GapState());
+	
+	private final BaseState state;
+	
+	private StateName(BaseState state) {
+		this.state = state;
+	}
+	
+	public StateName handleState()
+    		throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException, RobotErrorException {
+		return this.state.handleState();
+	}
 }
