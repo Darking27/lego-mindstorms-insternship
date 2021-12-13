13ac4dda402d20f95c9889d7879eb25e030f08d9
Niklas Arlt <n.a@live.de>
2021-12-13 11:30:54 +0100

    Change RobotUtils to no longer use isMoving

--- src/framework/RobotUtils.java
+++ src/framework/RobotUtils.java
@@ -3,6 +3,7 @@ package framework;
 import exceptions.KeyPressedException;
 import exceptions.MenuException;
 import exceptions.StopException;
+import lejos.robotics.RegulatedMotor;
 
 public final class RobotUtils {
 
@@ -19,12 +20,11 @@ public final class RobotUtils {
 		resetTachos();
 		
 		int rotation = 465 * degrees / 90;
-
-		Ports.LEFT_MOTOR.rotate((int) (0.98*rotation), true);
-		Ports.RIGHT_MOTOR.rotate(-rotation, false);
-
-		Ports.LEFT_MOTOR.stop(true);
-		Ports.RIGHT_MOTOR.stop(false);
+		
+		if (rotation < 0)
+			turn(Math.abs(rotation), false);
+		else
+			turn(rotation, true);
 	}
 
 	public static void turnToNeutralTacho() throws KeyPressedException {
@@ -33,32 +33,19 @@ public final class RobotUtils {
 	}
 	
 	public static void driveStraight(int distance) throws KeyPressedException {
-		driveStraight(distance, Integer.MAX_VALUE);
-	}
-
-	public static void driveStraight(int distance, int speed) throws KeyPressedException {
-		if (speed == Integer.MAX_VALUE) {
-			setMaxSpeed();
-		} else {
-			setSpeed(speed);
-		}
-		
-		int rotation = distance;
-		Ports.LEFT_MOTOR.rotate(rotation, true);
-		Ports.RIGHT_MOTOR.rotate(rotation, true);
-
-		checkForKeyPressAsLongAsMoving();
-
-		stopMotors();
-	}
-
-	public static void checkForKeyPressAsLongAsMoving() throws KeyPressedException {
-		while (Ports.LEFT_MOTOR.isMoving() || Ports.RIGHT_MOTOR.isMoving()) {
-			checkForKeyPress();
-		}
+		if (distance < 0)
+			driveStraight(Math.abs(distance), false);
+		else
+			driveStraight(distance, true);
 	}
 	
-	public static void checkForKeyPress() throws KeyPressedException{
+	public static void checkForKeyPress() throws KeyPressedException {
+		if (Ports.ESCAPE.isDown() || Ports.ENTER.isDown()) {
+			Ports.ULTRASONIC_MOTOR.setSpeed(80);
+			Ports.ULTRASONIC_MOTOR.rotateTo(0);
+			RobotUtils.stopMotors();
+		}
+		
 		if (Ports.ESCAPE.isDown())
 			throw new StopException();
 		if (Ports.ENTER.isDown())
@@ -88,5 +75,77 @@ public final class RobotUtils {
 		Ports.LEFT_MOTOR.forward();
 		Ports.RIGHT_MOTOR.forward();
 	}
+	
+	public static void driveStraight(int encoderValue, boolean forward)
+    		throws KeyPressedException {    	
+    	resetTachos();
+        
+        int leftTachoCount = 0;
+        int rightTachoCount = 0;
+        
+        if (forward) {
+	        Ports.LEFT_MOTOR.forward();
+	        Ports.RIGHT_MOTOR.forward();
+        } else {
+        	Ports.LEFT_MOTOR.backward();
+	        Ports.RIGHT_MOTOR.backward();
+        }
+        
+        while (Math.abs(rightTachoCount) <= encoderValue || Math.abs(rightTachoCount) <= encoderValue) {        	
+            if (Math.abs(rightTachoCount) >= encoderValue) {
+                Ports.RIGHT_MOTOR.stop(true);
+            }
+            if (Math.abs(leftTachoCount) >= encoderValue) {
+                Ports.LEFT_MOTOR.stop(true);
+            }
+            
+            checkForKeyPress();
+            
+            leftTachoCount = Ports.LEFT_MOTOR.getTachoCount();
+            rightTachoCount = Ports.RIGHT_MOTOR.getTachoCount();
+        }
+        
+        Ports.LEFT_MOTOR.stop(true);
+        Ports.RIGHT_MOTOR.stop();
+    }
+	
+    public static void turn(int encoderValue, boolean right)
+    		throws KeyPressedException {        
+        resetTachos();
+        
+        RegulatedMotor m1;
+        RegulatedMotor m2;
+        
+        if (right) {
+            m1 = Ports.LEFT_MOTOR;
+            m2 = Ports.RIGHT_MOTOR;
+        } else {
+            m1 = Ports.RIGHT_MOTOR;
+            m2 = Ports.LEFT_MOTOR;
+        }
+        
+        int m1TachoCount = 0;
+        int m2TachoCount = 0;
+        
+        m1.forward();
+        m2.backward();
+        
+        while (m1TachoCount <= encoderValue || m2TachoCount <= encoderValue) {
+        	if (m1TachoCount >= encoderValue) {
+                m1.stop(true);
+            }
+            if (m2TachoCount >= encoderValue) {
+                m2.stop(true);
+            }
+            
+            checkForKeyPress();
+            
+            m1TachoCount = Math.abs(m1.getTachoCount());
+            m2TachoCount = Math.abs(m2.getTachoCount());
+        }
+        
+        m1.stop(true);
+        m2.stop();
+    }
 }
 
