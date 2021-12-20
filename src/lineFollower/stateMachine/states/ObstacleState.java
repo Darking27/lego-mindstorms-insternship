bae4bdc7a7c1bcd27a03a4a6f0d217bde5ac959c
Felix Rohn <felix.rohn@student.kit.edu>
2021-12-20 11:01:42 +0100

    The robot now drives faster around the obstacle

--- src/lineFollower/stateMachine/states/ObstacleState.java
+++ src/lineFollower/stateMachine/states/ObstacleState.java
@@ -24,13 +24,13 @@ public class ObstacleState extends BaseState {
 	    Ports.LEFT_MOTOR.rotate(-100, true);
 	    Ports.RIGHT_MOTOR.rotate(-100, false);
 	    
-		driveBackwardStraight(400, false, true);
+		driveBackwardStraight(400, 600, false, true);
 		turnLeft(200, false, true);
-		driveForwardStraight(900, false, true);
+		driveForwardStraight(900, 600, false, true);
 		turnRight(200, false, true);
-		driveForwardStraight(900, false, true);
+		driveForwardStraight(800, 600, false, true);
 		turnRight(200, false, true);
-		if (driveForwardStraight(1500, true, true)) {
+		if (driveForwardStraight(1500, 500, true, true)) {
 			return StateName.FOLLOW_LINE;
 		} else {
 			throw new RobotErrorException("Robot can not find line after obstacle");
