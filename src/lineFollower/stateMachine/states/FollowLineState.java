df8282c44fb4c03b5aa193ed918cedd80f56dd50
Felix Rohn <felix.rohn@student.kit.edu>
2021-12-20 11:01:00 +0100

    Added new Controller (Regler)

--- src/lineFollower/stateMachine/states/FollowLineState.java
+++ src/lineFollower/stateMachine/states/FollowLineState.java
@@ -18,7 +18,7 @@ public class FollowLineState extends BaseState {
 	public StateName handleState()
 			throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
 		
-		slowDownRegulator();
+		newController();
 		return StateName.SEARCH_LINE;
 	}
 	
@@ -31,8 +31,8 @@ public class FollowLineState extends BaseState {
 	private void slowDownRegulator()
 			throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
 		
-		double Kp = 950;  //900
-		double Kd = 500;  //700
+		double Kp = 900;  //900
+		double Kd = 700;  //700
 		
 		double targetValue = 0.5;
         double defaultSpeed = 400;  //400
@@ -91,74 +91,75 @@ public class FollowLineState extends BaseState {
 	}
 	
 	@SuppressWarnings("unused")
-	private void normalRegulator()
+	private void newController()
 			throws ProcessInteruptedEnterException, RobotCollisionException, FinishLineException {
 		
-		double KpLeft = 600;			//To turn away from the line (should be greater than KpRight)
-		double KpRight = 400;
-		
-		double targetValue = 0.4;
-		double defaultSpeed = 300;
-		int speedCap = 400;
-		
-		float[] sample = new float[autoAdjustRGBFilter.sampleSize()];
-		autoAdjustRGBFilter.fetchSample(sample, 0);
-		
-		double currentValue = AutoAdjustFilter.getGrayValue(sample);
-	    int leftSpeed;
-	    int rightSpeed;
-		
-		Ports.LEFT_MOTOR.setSpeed((int) defaultSpeed);
-		Ports.RIGHT_MOTOR.setSpeed((int) defaultSpeed);
-		
-		Ports.LEFT_MOTOR.forward();
-		Ports.RIGHT_MOTOR.forward();
-		
-		while (!lineLost(currentValue)) {
-        	checkRobotInputs(sample, true);
-        	
-        	double diff = currentValue - targetValue;
+	    double Kp = 900;  //900
+        double Kd = 500;  //700
+        
+        double targetValue = 0.5;
+        double defaultSpeed = 400;  //400
+        
+        float[] sample = new float[autoAdjustRGBFilter.sampleSize()];
+        autoAdjustRGBFilter.fetchSample(sample, 0);
+        
+        double currentValue = AutoAdjustFilter.getGrayValue(sample);;
+        int leftSpeed;
+        int rightSpeed;
+        
+        double oldSpeed = defaultSpeed / 4;
+        
+        Ports.LEFT_MOTOR.setSpeed((int) oldSpeed);
+        Ports.RIGHT_MOTOR.setSpeed((int) oldSpeed);
+        
+        Ports.LEFT_MOTOR.forward();
+        Ports.RIGHT_MOTOR.forward();
+        
+        while (!lineLost(currentValue)) {
+            checkRobotInputs(sample, true);
+            
+            double diff = currentValue - targetValue;
+            double speed = defaultSpeed - Math.abs(diff * Kd);
+            
+            double factor = 3;
+            if (speed > factor + oldSpeed) {
+                speed = factor + oldSpeed;
+                System.out.println("slow increase: " + (int) oldSpeed + " --> " + (int) speed);
+            }
+            oldSpeed = speed;
+            
             if (diff >= 0) {
-	            leftSpeed = (int) (defaultSpeed - Math.abs(diff * KpLeft));
-	            rightSpeed = (int) (defaultSpeed + Math.abs(diff * KpLeft));
+                leftSpeed = (int) (speed - Math.abs(diff * Kp));
+                rightSpeed = (int) (speed + Math.abs(diff * Kp));
             } else {
-            	leftSpeed = (int) (defaultSpeed + Math.abs(diff * KpRight));
-	            rightSpeed = (int) (defaultSpeed - Math.abs(diff * KpRight));
+                leftSpeed = (int) (speed + Math.abs(diff * Kp));
+                rightSpeed = (int) (speed - Math.abs(diff * Kp));
             }
             
-            leftSpeed = limitSpeed(leftSpeed, speedCap);
-            rightSpeed = limitSpeed(rightSpeed, speedCap);
+            if (leftSpeed < 0) {
+                rightSpeed = rightSpeed / 2;
+            }
+            if (rightSpeed < 0) {
+                leftSpeed = leftSpeed / 2;
+            }
             
             Ports.LEFT_MOTOR.setSpeed(Math.abs(leftSpeed));
             Ports.RIGHT_MOTOR.setSpeed(Math.abs(rightSpeed));
             
             if (leftSpeed >= 0) {
-            	Ports.LEFT_MOTOR.forward();
+                Ports.LEFT_MOTOR.forward();
             } else {
-            	Ports.LEFT_MOTOR.backward();
+                Ports.LEFT_MOTOR.backward();
             }
             if (rightSpeed >= 0) {
-            	Ports.RIGHT_MOTOR.forward();
+                Ports.RIGHT_MOTOR.forward();
             } else {
-            	Ports.RIGHT_MOTOR.backward();
+                Ports.RIGHT_MOTOR.backward();
             }
-        	
-        	autoAdjustRGBFilter.fetchSample(sample, 0);
-        	currentValue = AutoAdjustFilter.getGrayValue(sample);
-		}
-		
-		Ports.RIGHT_MOTOR.rotate(50);
-	}
-	
-	private int limitSpeed(int targetSpeed, int speedCap) {
-		if (Math.abs(targetSpeed) > speedCap) {
-			if (targetSpeed >= 0) {
-				return speedCap;
-			} else {
-				return -speedCap;
-			}
-		}
-		return targetSpeed;
+            
+            autoAdjustRGBFilter.fetchSample(sample, 0);
+            currentValue = AutoAdjustFilter.getGrayValue(sample);
+        }
 	}
 	
 	/**
